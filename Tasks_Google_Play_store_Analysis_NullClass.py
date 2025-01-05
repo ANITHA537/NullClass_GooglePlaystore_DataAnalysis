ecb91cb7f80c5f5eb4b7e24752326eda453be08d
ANITHA537 <98999981+ANITHA537@users.noreply.github.com>
2025-01-05 15:09:37 +0530

    Update Tasks_Google_Play_store_Analysis_NullClass.py

--- Tasks_Google_Play_store_Analysis_NullClass.py
+++ Tasks_Google_Play_store_Analysis_NullClass.py
@@ -391,33 +391,52 @@ dashboard_html = """
             const secondMessage = secondPlotContainer.querySelector(".message");
             const secondPlot = secondPlotContainer.querySelector(".plot");
             const secondInsights = secondPlotContainer.querySelector(".insights");
-            
+            firstMessage.style.display = "block";
+            firstPlot.style.display = "none";
+            firstInsights.style.display = "none";
+    
+            secondMessage.style.display = "block";
+            secondPlot.style.display = "none";
+            secondInsights.style.display = "none";
+
+            if (currentHours >= 15 && currentHours < 17) {{
+                firstMessage.style.display = "none";
+                firstPlot.style.display = "block";
+                firstInsights.style.display = "block"; 
+                secondMessage.style.display = "block";
+            }}
+            else if (currentHours >= 17 && currentHours < 19) {{
+                secondMessage.style.display = "none";
+                secondPlot.style.display = "block";
+                secondInsights.style.display = "block"; 
+                firstMessage.style.display = "block";
+            }}
 
     
-                if (currentHours >= 15 && currentHours < 17) {{
-                    firstMessage.style.display = "none";
-                    firstPlot.style.display = "block";
-                    firstInsights.style.display = "block";
-                    secondMessage.style.display = "block";
-                    secondPlot.style.display = "none";
-                    secondInsights.style.display = "none";
-                }}
-                else if (currentHours >= 17 && currentHours < 19) {{
-                    secondMessage.style.display = "none";
-                    secondPlot.style.display = "block";
-                    secondInsights.style.display = "block";
-                    firstMessage.style.display = "block";
-                    firstPlot.style.display = "none";
-                    firstInsights.style.display = "none";
-                }}
-                else{{
-                    firstMessage.style.display = "block";
-                    firstPlot.style.display = "none";
-                    firstInsights.style.display = "none";
-                    secondMessage.style.display = "block";
-                    secondPlot.style.display = "none";
-                    secondInsights.style.display = "none";
-                }}
+               // if (currentHours >= 15 && currentHours < 17) {{
+                  //  firstMessage.style.display = "none";
+                   // firstPlot.style.display = "block";
+                   // firstInsights.style.display = "block";
+                   // secondMessage.style.display = "block";
+                   // secondPlot.style.display = "none";
+                   // secondInsights.style.display = "none";
+               // }}
+               // else if (currentHours >= 17 && currentHours < 19) {{
+                    //secondMessage.style.display = "none";
+                   // secondPlot.style.display = "block";
+                    //secondInsights.style.display = "block";
+                   // firstMessage.style.display = "block";
+                    //firstPlot.style.display = "none";
+                   // firstInsights.style.display = "none";
+                //}}
+               // else{{
+                    //firstMessage.style.display = "block";
+                    //firstPlot.style.display = "none";
+                   // firstInsights.style.display = "none";
+                    //secondMessage.style.display = "block";
+                    //secondPlot.style.display = "none";
+                    //secondInsights.style.display = "none";
+                //}}
                 
                 
             }}
